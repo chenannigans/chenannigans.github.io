b110c1b59e994a8c541a2699dedba1fb9ac92a48
Ann Chen <annchen@andrew.cmu.edu>
2017-01-13 01:39:45 -0800

    projects rts redesign and update

--- js/script.js
+++ js/script.js
@@ -1,11 +1,35 @@
-var $window = $(window);
+// var $window = $(window);
 	
-	$window.on('scroll', function(e) {
-		var scrollTop = $window.scrollTop();
-		$('.nav-wrapper').toggleClass('#14b2c6', scrollTop > 10);
-		$('nav ul a').toggleClass('white-text', scrollTop > 10);
-		$('nav .brand-logo').toggleClass('white-text', scrollTop > 10);
-		$('.nav-wrapper').toggleClass('white', scrollTop < 10);
+// 	$window.on('scroll', function(e) {
+// 		var scrollTop = $window.scrollTop();
+// 		$('.nav-wrapper').toggleClass('altnav', scrollTop > 10);
+// 		// $('nav ul a').toggleClass('white-text', scrollTop > 10);
+// 		// $('nav .brand-logo').toggleClass('white-text', scrollTop > 10);
+// 		// $('.nav-wrapper').toggleClass('white', scrollTop < 10);
 
 
-			});
+// 			});
+
+//  $('.table-of-contents').each(function() {
+//     var $this = $(this);
+//     var $target = $('#' + $(this).attr('data-target'));
+//     $this.pushpin({
+//       top: $target.offset().top,
+//       bottom: $target.offset().top + $target.outerHeight() - $this.height()
+//     });
+//   });
+
+//   $(document).ready(function(){
+//     $('.scrollspy').scrollSpy(scrollOffset: 500);
+//     scrollOffset(500);
+//     $('.material-tooltip').tooltip({delay: 50});
+
+
+//     $('.target').pushpin({
+//       top: 1000,
+//       bottom: 0,
+//       offset: 0
+//     });
+//   });
+
+//       
